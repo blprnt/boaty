8a8fe0a52dbf9ba95b54c4b94675f4981bfb9a46
Jer Thorp <blprnt@blprnt.com>
2019-05-29 13:57:51 -0400

    MMSI connected to marinetraffic.com

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const raspi = require('raspi');
 const Serial = require('raspi-serial').Serial;
 var AisParser = require('aisparser');
 const https = require('https')
+var http = require('http'),
+fs = require('fs');
 var parser = new AisParser();
 
 var buff = "";
@@ -9,6 +11,7 @@ var buff = "";
 var testMMSI = "367782880";
 
 //Monitor the serial port for the AIS receiver
+
 raspi.init(() => {
 	var serial = new Serial({portId:'/dev/serial0', baudRate:38400});
 	serial.open(() => {
@@ -47,74 +50,25 @@ function parseAIS(msg) {
 }
 
 //Get vessel detail
-
+//https://www.marinetraffic.com/en/ais/details/ships/mmsi:367782880
 function getVesselDetails(mmsi) {
+	https.get("https://www.marinetraffic.com/en/ais/details/ships/mmsi:" + mmsi, (resp) => {
+	  let data = '';
 
-	const data = JSON.stringify({
-	  	'lang':'en',
-		'lgg':7,
-		'p':1,
-		'sh_name':'',
-		'sh_callsign':'',
-		'sh_mmsi': mmsi,
-		'cgaid':0,
-		'sh_epirb_id':'',
-		'sh_epirb_hex':'',
-		'sh_imo_nbr':''
-	})
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+	    data += chunk;
+	  });
 
-	const options = {
-	  hostname: 'www.itu.int',
-	  port: 443,
-	  path: '/online/mms/mars/ship_search.sh',
-	  method: 'POST',
-	  headers: {
-	    'Content-Type': 'application/json',
-	    'Content-Length': data.length
-	  }
-	}
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+	    var t = data.split("var ls_vessel  = ")[1].split("/*")[0];
+	    var j = JSON.parse(t);
+	    return(j);
+	  });
+});
 
-	const req = https.request(options, (res) => {
-	  console.log(`statusCode: ${res.statusCode}`)
-
-	  res.on('data', (d) => {
-	    process.stdout.write(d)
-	  })
-	})
-
-	req.on('error', (error) => {
-	  console.error(error)
-	})
-
-	req.write(data)
-	req.end()
 }
 
 
 
-/*
-
-def get_vessel_details(mmsi):
-	# look up vessel name from the vessel's Mobile Maritime Subscriber Identifier (mmsi)
-	data= {
-	    'lang':'en',
-		'lgg':7,
-		'p':1,
-		'sh_name':'',
-		'sh_callsign':'',
-		'sh_mmsi': mmsi,
-		'cgaid':0,
-		'sh_epirb_id':'',
-		'sh_epirb_hex':'',
-		'sh_imo_nbr':''
-	    }
-	r = requests.post('http://www.itu.int/online/mms/mars/ship_search.sh', data=data)
-
-	page = r.text.split("\n")
-	for x in page:
-		if "<A HREF=ship_detail.sh" in x:
-			link = "http://www.itu.int/online/mms/mars/" + x.split("<A HREF=")[1].split(">")[0]
-			print link
-			vessel_name = link.split("&")[2].replace("+"," ")
-			return vessel_name
-*/
